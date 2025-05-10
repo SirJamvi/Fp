c6a57b0dfbf2f61085de38696b6015e732a7a042
BayuSyahputra1194 <if23.bayusyahputra@mhs.ubpkarawang.ac.id>
2025-05-11 01:10:46 +0700

    Landing Page Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -715,7 +715,7 @@
   </div>
 </footer>
 
-<!-- JavaScript Libraries -->
+<!-- JavaScript Libraries -->>> 
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
